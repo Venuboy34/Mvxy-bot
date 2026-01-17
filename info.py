b036a3cb940c498c852bf04973e7d0948d645cf9
Venuboy34 <venura505@gmail.com>
2026-01-17 15:30:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ INDEX_CAPTION = True # Save caption db when idexing make it False if you dont us
 COVERX = True # Use cover image for indexed files (default: True)
 # If you disable it then bot will use a default thumb for all files
 
-PICS_URL = [https://api.aniwallpaper.workers.dev/random?type=boy] #random anime girl img each time from aniwallpaper (Experimental)
+PICS_URL = ["https://api.aniwallpaper.workers.dev/random?type=boy"] #random anime girl img each time from aniwallpaper (Experimental)
 PICS = ['https://i.ibb.co/ksrvqFFw/img-8312532076.jpg', 'https://i.ibb.co/LhrhYmjz/img-8312532076.jpg', 'https://i.ibb.co/DDwXh1hJ/img-8312532076.jpg', 'https://i.ibb.co/HLsntHQm/img-8312532076.jpg']  # Sample pic
 NOR_IMG = "https://graph.org/file/e20b5fdaf217252964202.jpg"
 MELCOW_PHOTO = "https://graph.org/file/56b5deb73f3b132e2bb73.jpg"
